c1560e6a20635b6a4eddbda53270607c83bd05ff
Liam Young <liam.young@canonical.com>
2016-09-22 14:52:44 +0000

    Initial hardcoded mess that works

--- mistralclient/auth/keystone.py
+++ mistralclient/auth/keystone.py
@@ -29,43 +29,67 @@ def authenticate(mistral_url=None, username=None,
             'Only user name or user id should be set'
         )
 
-    keystone_client = _get_keystone_client(auth_url)
+    auth_args = {
+        'username': username,
+        'user_id': user_id,
+        'password': api_key,
+        'auth_url': auth_url,
+    }
+    api_version = _get_keystone_api_version(auth_url)
+    if api_version == 2:
+        auth_args.update({
+            'tenant_id': project_id,
+            'cacert': cacert,
+            'endpoint': auth_url,
+            'insecure': insecure,
+            'token': auth_token,
+            'tenant_name': project_name})
+    else: 
+        auth_args.update({
+            'project_domain_name': 'Default',
+            'user_domain_name': 'admin_domain',
+            'project_name': "admin"})
 
-    keystone = keystone_client.Client(
-        username=username,
-        user_id=user_id,
-        password=api_key,
-        token=auth_token,
-        tenant_id=project_id,
-        tenant_name=project_name,
-        auth_url=auth_url,
-        endpoint=auth_url,
-        cacert=cacert,
-        insecure=insecure
-    )
-
-    keystone.authenticate()
-
-    token = keystone.auth_token
+    keystone = _get_keystone_client(**auth_args)
+        
+    #token = keystone.auth_token
     user_id = keystone.user_id
     project_id = keystone.project_id
 
     if not mistral_url:
-        try:
-            mistral_url = keystone.service_catalog.url_for(
-                service_type=service_type,
-                endpoint_type=endpoint_type
-            )
-        except Exception:
-            mistral_url = None
+        mistral_svc_id = keystone.services.find(name='mistral').id
+        ep = keystone.endpoints.find(service_id=mistral_svc_id, interface='public')
+        print("LY {}".format(ep))
+        mistral_url = ep.url
+        
+#        try:
+#            mistral_url = keystone.service_catalog.url_for(
+#                service_type=service_type,
+#                endpoint_type=endpoint_type
+#            )
+#        except Exception:
+#            mistral_url = None
 
+    token = keystone.auth_token
+    print("TOKEN: {}".format(token))
     return mistral_url, token, project_id, user_id
 
 
-def _get_keystone_client(auth_url):
-    if "v2.0" in auth_url:
+def _get_keystone_api_version(auth_url):
+    return 2 if "v2.0" in auth_url else 3
+    
+def _get_keystone_client(**kwargs):
+    api_version = _get_keystone_api_version(kwargs['auth_url'])
+    if api_version == 2:
         from keystoneclient.v2_0 import client
+        keystone = keystone_client.Client(**kwargs)
     else:
-        from keystoneclient.v3 import client
-
-    return client
+        from keystoneclient.v3 import client as ksclient_v3
+        keystone = ksclient_v3.Client(**kwargs)
+#        from keystoneauth1.identity import v3
+#        from keystoneauth1 import session
+#        from keystoneclient.v3 import client
+#        auth = v3.Password(**kwargs)
+#        sess = session.Session(auth=auth)
+#        keystone = client.Client(session=sess)
+    return keystone
